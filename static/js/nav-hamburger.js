53a136e995335d9bfb6b63b4a7437515ce692612
Jonty Morris <jonty.morris@yahoo.co.nz>
2017-10-18 13:54:11 +1300

    Removed the hamburger.js
    This file was removed because it is not used.
    https://github.com/charissayj/portfolio/issues/6

--- static/js/nav-hamburger.js
+++
@@ -1,9 +0,0 @@
-// toggle between adding and removing the responsive class to topnav when the user clicks on the icon
-function navCollapse() {
-    var nav = document.getElementById("collapse");
-    if (nav.className === "topnav") {
-        nav.className += " responsive";
-    } else {
-        nav.className = "topnav";
-    }
-}
